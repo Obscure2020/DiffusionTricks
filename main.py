bf0dd3942236f5275b35db8f1189c17d21d2bfa8
Joey Sodergren <jas.elgoog@gmail.com>
2025-05-04 17:50:45 -0400

    Rewrite image recombination

--- main.py
+++ main.py
@@ -46,6 +46,33 @@ def transform_img_halve_undo(arr_obj):
     roll_amount = -1 * (int(arr_obj.shape[axis] / 2))
     return np.roll(arr_obj, roll_amount, axis)
 
+def recombine_images(img_a, img_b):
+    img_check(img_a)
+    img_check(img_b)
+    assert img_a.shape[0] == 512, "Unexpected image dimensions."
+    assert img_a.shape[1] == 512, "Unexpected image dimensions."
+    assert img_b.shape[0] == 512, "Unexpected image dimensions."
+    assert img_b.shape[1] == 512, "Unexpected image dimensions."
+    img_a_64 = img_a.astype(np.float64)
+    img_b_64 = img_b.astype(np.float64)
+    img_mean = np.mean([img_a_64, img_b_64], axis=0)
+    red_min = img_mean[:, :, 0].min()
+    red_max = img_mean[:, :, 0].max()
+    red_scale = red_max - red_min
+    green_min = img_mean[:, :, 1].min()
+    green_max = img_mean[:, :, 1].max()
+    green_scale = green_max - green_min
+    blue_min = img_mean[:, :, 2].min()
+    blue_max = img_mean[:, :, 2].max()
+    blue_scale = blue_max - blue_min
+    img_result = np.zeros((512, 512, 3), dtype=np.float64)
+    for y in range(512):
+        for x in range(512):
+            img_result[y, x, 0] = (img_mean[y, x, 0] - red_min) / red_scale
+            img_result[y, x, 1] = (img_mean[y, x, 1] - green_min) / green_scale
+            img_result[y, x, 2] = (img_mean[y, x, 2] - blue_min) / blue_scale
+    return img_result.astype(np.float32)
+
 prompt_a = "Albert Einstein, impressionist painting, 8k"
 prompt_b = "Marilyn Monroe, impressionist painting, 8k"
 strength_schedule = [0.98, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
@@ -71,9 +98,11 @@ for i in range(len(steps_schedule)):
     img_a = pipeline(prompt_a, image=img_a, num_images_per_prompt=1, num_inference_steps=steps_schedule[i], strength=strength_schedule[i], guidance_scale=0.0, output_type="np").images[0]
     img_b = pipeline(prompt_b, image=img_b, num_images_per_prompt=1, num_inference_steps=steps_schedule[i], strength=strength_schedule[i], guidance_scale=0.0, output_type="np").images[0]
     img_b = untransform_img(img_b)
-    img_mean = np.mean([img_a, img_b], axis=0)
-    img_a = img_mean
-    img_b = transform_img(img_mean)
+    img_smash = recombine_images(img_a, img_b)
+    img_a = img_smash
+    img_b = transform_img(img_smash)
+    write_PNG_from_ndarray(f"test_img_a_{i}.png", img_a)
+    write_PNG_from_ndarray(f"test_img_b_{i}.png", img_b)
 
 # Clean up pipeline memory after we're done using it
 del pipeline
@@ -81,8 +110,5 @@ gc.collect()
 if USE_CUDA:
     torch.cuda.empty_cache()
     torch.cuda.ipc_collect()
-
-write_PNG_from_ndarray("test_img_a.png", img_a)
-write_PNG_from_ndarray("test_img_b.png", img_b)
 
 print("Work complete. Cleaning up and exiting...")
